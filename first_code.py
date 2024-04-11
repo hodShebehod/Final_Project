e63bc79e5d22d5982af7c36578d0f24f1b0d7c85
hodShebehod <155921015+hodShebehod@users.noreply.github.com>
2024-04-11 05:27:11 +0000

    first try

--- first_code.py
+++ first_code.py
@@ -22,8 +22,27 @@ def main():
         cursor_obj.execute(grades_table)
     except Exception as e:
         print(e.message)
-
+'''this is the place to call the methods if the buttons are pressed'''
+    # close the object at the end
     connection_obj.close()
+
+def create():
+    '''execute code to create a new record in the student table and also grades table'''
+    # create a new student into student table
+    create_new_student = """ INSERT INTO students values(, [Enter first name], [Enter last name])"""
+    cursor_obj.execute(create_new_student)
+
+def read():
+    '''execute code to show table of students and names and grades and average'''
+    # show a list of students and their average grades
+    read_tables = """ SELECT * FROM students and average FROM grades"""
+    cursor_obj.execute(read_tables)
+
+def update():
+    '''execute code to enable editing to students grade'''
+
+def update():
+    '''execute code to delete a record'''
 
 if __name__ == "__main__":
     main()
