6fb4e1b5f86d1b557bc5e1561340526bd1f4032b
hodShebehod <155921015+hodShebehod@users.noreply.github.com>
2024-04-18 00:54:35 +0000

    following along with the vedio of how to make apps

--- app.py
+++ app.py
@@ -5,10 +5,14 @@ from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
-@app.get("/")
+'''@app.get("/")
 def home():
     return 'Hello, World!'
 
 @app.get("/bye")
 def bye():
-    return 'Bye, World!'
+    return 'Bye, World!''''
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(basedir, 'project.sqlite')
